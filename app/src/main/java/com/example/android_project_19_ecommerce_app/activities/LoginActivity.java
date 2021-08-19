498edd7f8ed1cae8fa01c88117b454692b48910e
ahmad montazeri <amontazeri53@yahoo.com>
2021-08-19 12:08:47 +0430

    mew

--- app/src/main/java/com/example/android_project_19_ecommerce_app/activities/LoginActivity.java
+++ app/src/main/java/com/example/android_project_19_ecommerce_app/activities/LoginActivity.java
@@ -2,14 +2,89 @@ package com.example.android_project_19_ecommerce_app.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import com.mobsandgeeks.saripaar.ValidationError;
+import com.mobsandgeeks.saripaar.Validator;
+import com.mobsandgeeks.saripaar.annotation.Checked;
+import com.mobsandgeeks.saripaar.annotation.ConfirmPassword;
+import com.mobsandgeeks.saripaar.annotation.Email;
+import com.mobsandgeeks.saripaar.annotation.Length;
+import com.mobsandgeeks.saripaar.annotation.Max;
+import com.mobsandgeeks.saripaar.annotation.Min;
+import com.mobsandgeeks.saripaar.annotation.NotEmpty;
+import com.mobsandgeeks.saripaar.annotation.Password;
+import com.mobsandgeeks.saripaar.annotation.Pattern;
+import com.mobsandgeeks.saripaar.annotation.Url;
+
+import java.util.List;
 
 import com.example.android_project_19_ecommerce_app.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements Validator.ValidationListener, View.OnClickListener {
+
+    private Validator validator;
+
+    @NotEmpty
+    @Length(min = 3, max = 10)
+    private EditText ed_username;
+
+    @NotEmpty
+    @Password
+    @Pattern(regex = "((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{6,20})")
+    private EditText ed_password;
+
+    private Button btn_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        initViews();
+        validator = new Validator(this);
+        validator.setValidationListener(this);
+    }
+
+
+    private void initViews() {
+        ed_username = findViewById(R.id.ed_username);
+        ed_password = findViewById(R.id.ed_password);
+    }
+
+    @Override
+    public void onValidationSucceeded() {
+        Toast.makeText(this, "We oK", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onValidationFailed(List<ValidationError> errors) {
+        for (ValidationError error : errors) {
+            View view = error.getView();
+            String message = error.getCollatedErrorMessage(this);
+            if (view instanceof EditText) {
+                ((EditText) view).setError(message);
+            } else {
+                Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        String username = ed_username.getText().toString();
+        String password = ed_password.getText().toString();
+
+        validator.validate();
+        if (username.equalsIgnoreCase("pmk")) {
+            ed_username.setError(getText(R.string.username_already_exists));
+        }
     }
 }
--- app/src/main/java/com/example/android_project_19_ecommerce_app/activities/MainActivity.java
+++ app/src/main/java/com/example/android_project_19_ecommerce_app/activities/MainActivity.java
@@ -1,15 +1,68 @@
 package com.example.android_project_19_ecommerce_app.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.android_project_19_ecommerce_app.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("My Toolbar");
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.menuAbout:
+                Toast.makeText(this, "You clicked about", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.menuSettings:
+                Toast.makeText(this, "You clicked settings", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.menuLogout:
+                Toast.makeText(this, "You clicked logout", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+        return true;
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent login_activity = new Intent(this, LoginActivity.class);
+        Intent register_activity = new Intent(this, RegisterActivity.class);
+
+        switch (view.getId()) {
+            case R.id.btn_login:
+                startActivity(login_activity);
+                break;
+            case R.id.btn_register:
+                startActivity(register_activity);
+                break;
+        }
     }
 }
